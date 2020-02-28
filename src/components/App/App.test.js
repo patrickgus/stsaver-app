23e550ddec6e5c0b22be717ac489cdaf02eb263b
Patrick Gustafson <patrickgus@charter.net>
2020-02-28 10:39:27 -0800

    update App tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,17 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import renderer from "react-test-renderer";
 import App from "./App";
 
 describe("App component", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 
   it("renders the UI as expected", () => {
-    const tree = renderer.create(<App />).toJSON();
+    const tree = renderer
+      .create(
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
