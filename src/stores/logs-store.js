5c82819839063717d85c2d14ba9b52d888624e11
Patrick Gustafson <patrickgus@charter.net>
2020-02-28 15:29:03 -0800

    add start and end times

--- src/stores/logs-store.js
+++ src/stores/logs-store.js
@@ -1,35 +1,45 @@
 export default [
   {
     id: 1,
-    date: "2020-02-26T00:00:00+00:00",
+    date: "2-26-2020",
+    start: "5:00pm",
+    end: "7:00pm",
     hours: 2,
     media_type: "phone",
     breaks: 0
   },
   {
     id: 2,
-    date: "2020-02-26T00:00:00+00:00",
+    date: "2-26-2020",
+    start: "7:00am",
+    end: "12:00pm",
     hours: 5,
     media_type: "computer",
     breaks: 10
   },
   {
     id: 3,
-    date: "2020-02-25T00:00:00+00:00",
+    date: "2-25-2020",
+    start: "4:00pm",
+    end: "8:00pm",
     hours: 4,
     media_type: "tablet",
     breaks: 5
   },
   {
     id: 4,
-    date: "2020-02-25T00:00:00+00:00",
+    date: "2-25-2020",
+    start: "7:00pm",
+    end: "2:00pm",
     hours: 7,
     media_type: "computer",
     breaks: 15
   },
   {
     id: 5,
-    date: "2020-02-24T00:00:00+00:00",
+    date: "2-24-2020",
+    start: "2:00pm",
+    end: "6:00pm",
     hours: 4,
     media_type: "computer",
     breaks: 8
