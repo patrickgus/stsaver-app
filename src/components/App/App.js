b5fa1c955ad78a9c92ec06030dfb5ace109f8d44
Patrick Gustafson <patrickgus@charter.net>
2020-02-28 08:16:47 -0800

    create and import NavBar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
+import NavBar from "../NavBar/NavBar";
+import "./App.css";
 
-function App() {
-  return <main className="App">{/* content goes here */}</main>;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App__header">
+          <NavBar />
+        </header>
+        <main className="App__main"></main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import "./NavBar.css";
+
+class NavBar extends Component {
+  handleLogoutClick = () => {
+    this.props.history.push("/");
+  };
+
+  renderLogoutLink() {
+    return (
+      <div className="NavBar__logged-in">
+        <Link onClick={this.handleLogoutClick} to="/">
+          Logout
+        </Link>
+      </div>
+    );
+  }
+
+  renderLoginLink() {
+    return (
+      <div className="NavBar__not-logged-in">
+        <Link to="/">Log in</Link>
+        <Link to="/register">Register</Link>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <nav className="NavBar">
+        <h3>
+          <Link to="/activity">STSaver</Link>
+        </h3>
+        {this.renderLoginLink()}
+      </nav>
+    );
+  }
+}
+
+export default withRouter(NavBar);
