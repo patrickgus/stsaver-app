9a1d96f64a1cc947b2a10af20d6b515e6799a1d9
Patrick Gustafson <patrickgus@charter.net>
2020-02-28 14:41:39 -0800

    create dummy log store

---
+++ src/stores/logs-store.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    date: "2020-02-26T00:00:00+00:00",
+    hours: 2,
+    media_type: "phone",
+    breaks: 0
+  },
+  {
+    id: 2,
+    date: "2020-02-26T00:00:00+00:00",
+    hours: 5,
+    media_type: "computer",
+    breaks: 10
+  },
+  {
+    id: 3,
+    date: "2020-02-25T00:00:00+00:00",
+    hours: 4,
+    media_type: "tablet",
+    breaks: 5
+  },
+  {
+    id: 4,
+    date: "2020-02-25T00:00:00+00:00",
+    hours: 7,
+    media_type: "computer",
+    breaks: 15
+  },
+  {
+    id: 5,
+    date: "2020-02-24T00:00:00+00:00",
+    hours: 4,
+    media_type: "computer",
+    breaks: 8
+  }
+];
