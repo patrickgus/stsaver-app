5c6bcbd3160041f0a73f90b3dae16933acb454b0
Patrick Gustafson <patrickgus@charter.net>
2020-02-28 16:10:51 -0800

    add header element

--- src/routes/RegistrationPage/RegistrationPage.js
+++ src/routes/RegistrationPage/RegistrationPage.js
@@ -6,7 +6,9 @@ class RegistrationPage extends Component {
   render() {
     return (
       <div className="RegistrationPage">
-        <h2>Sign up for an account</h2>
+        <div className="RegistrationPage__header">
+          <h2>Sign up for an account</h2>
+        </div>
         <RegistrationForm />
       </div>
     );
