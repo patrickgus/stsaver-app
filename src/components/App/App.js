0947004939440a7eb6efc87532484b33c922f939
Patrick Gustafson <patrickgus@charter.net>
2020-03-04 20:40:54 +0000

    import auth idle and token services

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,49 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
+import PrivateRoute from "../Utils/PrivateRoute";
+import PublicOnlyRoute from "../Utils/PublicOnlyRoute";
 import NavBar from "../NavBar/NavBar";
 import LandingPage from "../../routes/LandingPage/LandingPage";
 import RegistrationPage from "../../routes/RegistrationPage/RegistrationPage";
 import ActivityPage from "../../routes/ActivityPage/ActivityPage";
 import AddLogPage from "../../routes/AddLogPage/AddLogPage";
 import TimerPage from "../../routes/TimerPage/TimerPage";
+import TokenService from "../../services/token-service";
+import AuthApiService from "../../services/auth-api-service";
+import IdleService from "../../services/idle-service";
 import "./App.css";
 
 class App extends Component {
+  state = { hasError: false };
+
+  static getDerivedStateFromError(error) {
+    console.error(error);
+    return { hasError: true };
+  }
+
+  componentDidMount() {
+    IdleService.setIdleCallback(this.logoutFromIdle);
+
+    if (TokenService.hasAuthToken()) {
+      IdleService.regiserIdleTimerResets();
+      TokenService.queueCallbackBeforeExpiry(() => {
+        AuthApiService.postRefreshToken();
+      });
+    }
+  }
+
+  componentWillUnmount() {
+    IdleService.unRegisterIdleResets();
+    TokenService.clearCallbackBeforeExpiry();
+  }
+
+  logoutFromIdle = () => {
+    TokenService.clearAuthToken();
+    TokenService.clearCallbackBeforeExpiry();
+    IdleService.unRegisterIdleResets();
+    this.forceUpdate();
+  };
+
   render() {
     return (
       <div className="App">
