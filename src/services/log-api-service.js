97d969d36aa6e87c7fd3ce39bfdd41c19ad464b7
Patrick Gustafson <patrickgus@charter.net>
2020-03-04 20:31:45 +0000

    create service object for api endpoints

---
+++ src/services/log-api-service.js
@@ -0,0 +1,67 @@
+import TokenService from "./token-service";
+import config from "../config";
+
+const LogApiService = {
+  getLogs(userId) {
+    return fetch(`${config.API_BASE_URL}/logs/${userId}`, {
+      headers: { authorization: `bearer ${TokenService.getAuthToken()}` }
+    }).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+    );
+  },
+
+  getTotalHours(userId) {
+    return fetch(`${config.API_BASE_URL}/logs/${userId}/hours`, {
+      headers: { authorization: `bearer ${TokenService.getAuthToken()}` }
+    }).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+    );
+  },
+
+  postLog(userId, start_time, end_time, media, breaks) {
+    return fetch(`${config.API_BASE_URL}/logs/${userId}`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        authorization: `bearer ${TokenService.getAuthToken()}`
+      },
+      body: JSON.stringify({
+        start_time,
+        end_time,
+        media,
+        breaks
+      })
+    }).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+    );
+  },
+
+  deleteLog(userId, logId) {
+    return fetch(`${config.API_BASE_URL}/logs/${userId}/${logId}`, {
+      method: "DELETE",
+      headers: { authorization: `bearer ${TokenService.getAuthToken()}` }
+    }).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+    );
+  },
+
+  updateLog(userId, logId, start_time, end_time, media, breaks) {
+    return fetch(`${config.API_BASE_URL}/logs/${userId}/${logId}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+        authorization: `bearer ${TokenService.getAuthToken()}`
+      },
+      body: JSON.stringify({
+        start_time,
+        end_time,
+        media,
+        breaks
+      })
+    }).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+    );
+  }
+};
+
+export default LogApiService;
