643f4657631976725e59a73c4a5f9ae9d1261dd4
Patrick Gustafson <patrickgus@charter.net>
2020-03-05 14:41:31 -0800

    hook up api to render logs

--- src/routes/ActivityPage/ActivityPage.js
+++ src/routes/ActivityPage/ActivityPage.js
@@ -1,22 +1,40 @@
 import React, { Component } from "react";
+import LogListContext from "../../contexts/LogListContext";
+import LogApiService from "../../services/log-api-service";
 import ActivityTracker from "../../components/ActivityTracker/ActivityTracker";
 import LogListItem from "../../components/LogListItem/LogListItem";
-import logs from "../../stores/logs-store";
 import "./ActivityPage.css";
 
 class ActivityPage extends Component {
+  static contextType = LogListContext;
+
+  componentDidMount() {
+    this.context.clearError();
+    LogApiService.getLogs()
+      .then(this.context.setLogList)
+      .catch(this.context.setError);
+  }
+
   renderLogs = () => {
-    return logs.map(log => <LogListItem key={log.id} log={log} />);
+    const { logList = [] } = this.context;
+    return logList.map(log => <LogListItem key={log.id} log={log} />);
   };
 
   render() {
+    const { error } = this.context;
     return (
       <div className="ActivityPage">
         <header className="ActivityPage__header">
           <h1>Screen Time Activity</h1>
           <ActivityTracker />
         </header>
-        <div className="ActivityPage__logs">{this.renderLogs()}</div>
+        <div className="ActivityPage__logs">
+          {error ? (
+            <p className="red">There was an error, try again</p>
+          ) : (
+            this.renderLogs()
+          )}
+        </div>
       </div>
     );
   }
