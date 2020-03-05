9c67544df615d5326135d6a4cb69433932725ca3
Patrick Gustafson <patrickgus@charter.net>
2020-03-05 08:16:45 -0800

    fix typo in API URL

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   API_BASE_URL:
     process.env.REACT_APP_API_BASE_URL ||
-    "http://localhost:8000//api",
+    "http://localhost:8000/api",
   TOKEN_KEY: "stsaver-client-auth-token"
 };
