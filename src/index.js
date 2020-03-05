04fd2dee87111bf550da528664ecdfa7073a0bd4
Patrick Gustafson <patrickgus@charter.net>
2020-03-05 14:28:48 -0800

    import LogListProvider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { LogListProvider } from "./contexts/LogListContext";
 import App from "./components/App/App";
 import "normalize.css";
 import "./index.css";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <LogListProvider>
+      <App />
+    </LogListProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
