38a205555ba189b9794333e6dae764d7f26eb1f3
Patrick Gustafson <patrickgus@charter.net>
2020-03-05 16:55:29 -0800

    hook up addLog form to api

--- src/components/AddLogForm/AddLogForm.js
+++ src/components/AddLogForm/AddLogForm.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
+import LogContext from "../../contexts/LogContext";
+import LogApiService from "../../services/log-api-service";
 import "./AddLogForm.css";
 
 class AddLogForm extends Component {
+  static contextType = LogContext;
+
   handleSubmit = e => {
     e.preventDefault();
-    this.props.history.push("/activity");
+    const { start_time, end_time, media, breaks } = e.target;
+
+    LogApiService.postLog(
+      start_time.value,
+      end_time.value,
+      media.value,
+      breaks.value
+    )
+      .then(this.context.addGame)
+      .then(() => {
+        start_time.value = "";
+        end_time.value = "";
+        media.value = "";
+        breaks.value = "";
+        this.props.history.push("/activity");
+      })
+      .catch(this.context.setError);
   };
 
   handleCancel = e => {
@@ -17,18 +37,21 @@ class AddLogForm extends Component {
     return (
       <form className="AddLogForm" onSubmit={this.handleSubmit}>
         <div className="AddLogForm__form-section">
-          <label htmlFor="date">Date</label>
-          <input type="date" name="date" id="date" required />
-        </div>
-        <div className="AddLogForm__form-section times">
-          <label htmlFor="start-time">Start time</label>
-          <input type="time" name="start-time" id="start-time" required />
-          <label htmlFor="end-time">End time</label>
-          <input type="time" name="end-time" id="end-time" required />
+          <label htmlFor="start_time">Start time</label>
+          <input
+            type="datetime-local"
+            name="start_time"
+            id="start_time"
+            required
+          />
         </div>
         <div className="AddLogForm__form-section">
-          <label htmlFor="media-type">Media type</label>
-          <select name="media-type" id="media-type" required>
+          <label htmlFor="end_time">End time</label>
+          <input type="datetime-local" name="end_time" id="end_time" required />
+        </div>
+        <div className="AddLogForm__form-section">
+          <label htmlFor="media">Media type</label>
+          <select name="media" id="media" required>
             <option value="">--Choose one--</option>
             <option value="computer">Computer</option>
             <option value="phone">Phone</option>
