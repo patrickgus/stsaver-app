a695834c92d5bb2821b5a97185a283878fb5270a
Patrick Gustafson <patrickgus@charter.net>
2020-03-06 09:48:43 -0800

    transfer styles to Timer component

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Timer.css";
 
 function Timer() {
   return (
-    <section className="TimerPage__timer">
+    <section className="Timer">
       <h3>00:00:00</h3>
-      <div className="TimerPage__buttons">
+      <div className="Timer__buttons">
         <button>Start</button>
         <button>Stop</button>
         <button>Reset</button>
