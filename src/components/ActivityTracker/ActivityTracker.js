2aee2053395aec721b240970bcf7a3d4e156173e
Patrick Gustafson <patrickgus@charter.net>
2020-03-06 11:36:00 -0800

    fetch logs data for activity chart

--- src/components/ActivityTracker/ActivityTracker.js
+++ src/components/ActivityTracker/ActivityTracker.js
@@ -1,24 +1,38 @@
-import React from "react";
-import ActivityChart from "../../images/Activity-chart-demo.PNG";
+import React, { Component } from "react";
+import LogContext from "../../contexts/LogContext";
+import LogApiService from "../../services/log-api-service";
+import ActivityChart from "../ActivityChart/ActivityChart";
 import "./ActivityTracker.css";
 
-function ActivityTracker() {
-  return (
-    <div className="ActivityTracker">
-      <h2>Activity Tracker</h2>
-      <div className="ActivityTracker__chart">
-        <img src={ActivityChart} alt="placeholder for activity tracker chart" />
+class ActivityTracker extends Component {
+  static contextType = LogContext;
+
+  componentDidMount() {
+    this.context.clearError();
+    LogApiService.getLogs()
+      .then(this.context.setLogList)
+      .catch(this.context.setError);
+  }
+
+  render() {
+    const { logList } =this.context;
+    return (
+      <div className="ActivityTracker">
+        <h2>Activity Tracker</h2>
+        <div className="ActivityTracker__chart">
+          <ActivityChart logs={logList} />
+        </div>
+        <dl className="ActivityTracker__stats">
+          <dt>Total Hours:</dt>
+          <dd>120</dd>
+          <dt>This week:</dt>
+          <dd>12</dd>
+          <dt>Weekly goal:</dt>
+          <dd>50</dd>
+        </dl>
       </div>
-      <dl className="ActivityTracker__stats">
-        <dt>Total Hours:</dt>
-        <dd>120</dd>
-        <dt>This week:</dt>
-        <dd>12</dd>
-        <dt>Weekly goal:</dt>
-        <dd>50</dd>
-      </dl>
-    </div>
-  );
+    );
+  }
 }
 
 export default ActivityTracker;
