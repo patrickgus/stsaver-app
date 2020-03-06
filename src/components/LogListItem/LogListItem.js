3733539ef8cff560c39cf17ae4c75280086d6ebf
Patrick Gustafson <patrickgus@charter.net>
2020-03-05 16:03:07 -0800

    format start and end times for logs

--- src/components/LogListItem/LogListItem.js
+++ src/components/LogListItem/LogListItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Moment from "react-moment";
 import "./LogListItem.css";
 
 function LogListItem(props) {
@@ -6,15 +7,15 @@ function LogListItem(props) {
 
   return (
     <section className="LogListItem">
-      <h3>{log.date}</h3>
+      <h3><Moment date={log.start_time} format="MM-DD-YYYY" /></h3>
       <p>
-        {log.start} - {log.end}
+        <Moment local date={log.start_time} format="h:mma" /> - <Moment local date={log.end_time} format="h:mma" />
       </p>
       <dl>
         <dt>Screen time hours:</dt>
         <dd>{log.hours}</dd>
         <dt>Media type:</dt>
-        <dd>{log.media_type}</dd>
+        <dd>{log.media}</dd>
         <dt>Breaks taken:</dt>
         <dd>{log.breaks}</dd>
       </dl>
