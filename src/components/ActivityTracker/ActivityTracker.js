1aa2ba7e76e5eaf9b38940d38242c6ec3af91c74
Patrick Gustafson <patrickgus@charter.net>
2020-03-06 16:34:34 -0800

    add Total hours to activity header

--- src/components/ActivityTracker/ActivityTracker.js
+++ src/components/ActivityTracker/ActivityTracker.js
@@ -7,16 +7,25 @@ class ActivityTracker extends Component {
   static contextType = LogContext;
 
   render() {
-    const { logList } =this.context;
+    const { error, logList, totalHours } = this.context;
+    console.log(totalHours[0]);
+    
     return (
       <div className="ActivityTracker">
         <h2>Activity Tracker</h2>
         <div className="ActivityTracker__chart">
+            {/* add filter for dates - subtract one week from today*/}
           <ActivityChart logs={logList} />
         </div>
         <dl className="ActivityTracker__stats">
           <dt>Total Hours:</dt>
-          <dd>120</dd>
+          <dd>
+            {error ? (
+              <p className="error">There was an error, try again</p>
+            ) : (
+              totalHours.sum
+            )}
+          </dd>
           <dt>This week:</dt>
           <dd>12</dd>
           <dt>Weekly goal:</dt>
