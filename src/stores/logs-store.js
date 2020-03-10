0ab63b28c51fdffed7bdd64b62da8b20efd1b85d
Patrick Gustafson <patrickgus@charter.net>
2020-03-09 22:46:53 -0700

    fix data in store

--- src/stores/logs-store.js
+++ src/stores/logs-store.js
@@ -1,47 +1,37 @@
 export default [
   {
     id: 1,
-    date: "2-26-2020",
-    start: "5:00pm",
-    end: "7:00pm",
-    hours: 2,
-    media_type: "Phone",
+    start_time: "2020-03-04 16:00",
+    end_time: "2020-03-04 20:00",
+    media: "television",
     breaks: 0
   },
   {
     id: 2,
-    date: "2-26-2020",
-    start: "7:00am",
-    end: "12:00pm",
-    hours: 5,
-    media_type: "Computer",
-    breaks: 10
+    start_time: "2020-03-05 7:00",
+    end_time: "2020-03-05 14:00",
+    media: "computer",
+    breaks: 15
   },
   {
     id: 3,
-    date: "2-25-2020",
-    start: "4:00pm",
-    end: "8:00pm",
-    hours: 4,
-    media_type: "Tablet",
+    start_time: "2020-03-05 16:00",
+    end_time: "2020-03-05 20:00",
+    media: "tablet",
     breaks: 5
   },
   {
     id: 4,
-    date: "2-25-2020",
-    start: "7:00pm",
-    end: "2:00pm",
-    hours: 7,
-    media_type: "Computer",
-    breaks: 15
+    start_time: "2020-03-06 7:00",
+    end_time: "2020-03-06 12:00",
+    media: "computer",
+    breaks: 10
   },
   {
     id: 5,
-    date: "2-24-2020",
-    start: "2:00pm",
-    end: "6:00pm",
-    hours: 4,
-    media_type: "Computer",
-    breaks: 8
+    start_time: "2020-03-06 17:00",
+    end_time: "2020-03-06 19:00",
+    media: "phone",
+    breaks: 0
   }
 ];
