d6e80ae8397f66c4832555e2fa1cd6fbac9292ae
Patrick Gustafson <patrickgus@charter.net>
2020-03-09 19:52:54 -0700

    filter logs for activity chart to show last week

--- src/components/ActivityTracker/ActivityTracker.js
+++ src/components/ActivityTracker/ActivityTracker.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import LogContext from "../../contexts/LogContext";
 import ActivityChart from "../ActivityChart/ActivityChart";
 import "./ActivityTracker.css";
+import { subDays } from "date-fns";
 
 class ActivityTracker extends Component {
   static contextType = LogContext;
@@ -12,8 +13,12 @@ class ActivityTracker extends Component {
       <div className="ActivityTracker">
         <h2>Activity Tracker</h2>
         <div className="ActivityTracker__chart">
-            {/* add filter for dates - subtract one week from today*/}
-          <ActivityChart logs={logList} />
+          <ActivityChart
+            logs={logList.filter(log => {
+              const date = new Date(log.start_time);
+              return date >= subDays(new Date(), 6) && date <= new Date();
+            })}
+          />
         </div>
         <dl className="ActivityTracker__stats">
           <dt>Total Hours:</dt>
