91ececcd9e399e53ea87a5ea413efac102c879b1
Patrick Gustafson <patrickgus@charter.net>
2020-03-18 07:48:30 -0700

    update tests and snapshots

--- src/components/LogListItem/LogListItem.test.js
+++ src/components/LogListItem/LogListItem.test.js
@@ -5,7 +5,7 @@ import renderer from "react-test-renderer";
 import LogListItem from "./LogListItem";
 import logs from "../../stores/logs-store";
 
-describe.only("LogListItem component", () => {
+describe("LogListItem component", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(
