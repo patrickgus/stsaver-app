d2412591290602db93f9234ab77eaf2a992b5ba3
Patrick Gustafson <patrickgus@charter.net>
2020-03-18 07:49:25 -0700

    add more test data to store

--- src/stores/logs-store.js
+++ src/stores/logs-store.js
@@ -1,9 +1,37 @@
 export default [
   {
     id: 1,
-    start_time: "2020-03-04T16:00:00",
-    end_time: "2020-03-04T20:00:00",
+    start_time: "2020-03-12T16:00:00",
+    end_time: "2020-03-12T20:00:00",
     media: "television",
     breaks: 0
+  },
+  {
+    id: 2,
+    start_time: "2020-03-13T08:00:00",
+    end_time: "2020-03-13T12:00:00",
+    media: "computer",
+    breaks: 12
+  },
+  {
+    id: 3,
+    start_time: "2020-03-13T18:00:00",
+    end_time: "2020-03-13T20:00:00",
+    media: "phone",
+    breaks: 0
+  },
+  {
+    id: 4,
+    start_time: "2020-03-14T07:00:00",
+    end_time: "2020-03-14T11:00:00",
+    media: "computer",
+    breaks: 10
+  },
+  {
+    id: 5,
+    start_time: "2020-03-14T15:00:00",
+    end_time: "2020-03-14T18:00:00",
+    media: "tablet",
+    breaks: 3
   }
 ];
